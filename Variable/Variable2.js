76932e6ed045385099965819c905966fdeb22305
hotaruZzZz <gfri5469@gmail.com>
2023-03-01 15:12:13 +0800

    variable

---
+++ Variable/Variable2.js
@@ -0,0 +1,20 @@
+var x = 1;
+let y = 2;
+const z = 3;
+
+
+{
+    var x = 'JavaScript';
+    let y = 'CSS';
+    const z = 'HTML';
+
+    console.log(x);
+    console.log(y);
+    console.log(z);
+    console.log('.'.repeat(60));
+}
+
+
+console.log(x);
+console.log(y);
+console.log(z);
